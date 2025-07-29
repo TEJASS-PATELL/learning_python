b825abc230cd934b2f4f209a69ad6689cd0a780b
Tejas <tejasspatell2@gmail.com>
2025-07-30 00:04:51 +0530

    first commit

--- project.py
+++ project.py
@@ -6,7 +6,20 @@ def allfile():
         print(f"{i+1} : {items}")
 
 def createfile():
+    try:
+        allfile()
+        name = input("Please enter the file name: ")
+        p = Path(name)
+        if not p.exists():
+            with open(p, "w") as fs:
+                data = input("Please enter your file content: ")
+                fs.write(data)
+                print("File created successfully.")
+        else:
+            print("This file already exists.")
     
+    except Exception as err:
+        print(f"Error occurred: {err}")
 
 print("Press 1 for creating a file");
 print("Press 2 for updating a file");
@@ -15,4 +28,5 @@ print("Press 4 for reading a file");
 
 check = int(input("Please tell your response :- "));
 
-if check == 1 : 
+if check == 1: 
+    createfile()
