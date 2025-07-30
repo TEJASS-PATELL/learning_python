778c58adc0b50fca6d5b636078ac163778284a4c
Tejas <tejasspatell2@gmail.com>
2025-07-31 00:37:33 +0530

    learning oops

--- oops.py
+++ oops.py
@@ -8,3 +8,23 @@ class Factory:
 
 print(Factory().a)
 Factory().hello()
+
+#! Objects-
+obj = Factory()
+print(obj.a)
+obj.hello()
+
+class Students:
+    def __init__(self, name, section, rollNo):   #!  Self is used for tracking location of objects.
+        self.name = name
+        self.section = section
+        self.rollNo = rollNo
+
+Student1 = Students("Tejas", "A", 54)
+Student2 = Students("Akshat", "A", 24)
+Student3 = Students("Prince", "B", 18)
+Student4 = Students("Pratik", "C", 66)
+
+print(Student4.name)
+print(Student2.rollNo, Student2.name)
+
