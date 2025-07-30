486cb04f8f77780aa3271390de138cd737a43639
Tejas <tejasspatell2@gmail.com>
2025-07-30 23:45:38 +0530

    basic project

--- project.py
+++ project.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import os
+
 def allfile():
     path = Path('')
     items = list(path.rglob("*"))
@@ -38,6 +40,52 @@ def readfile():
     except Exception as err:
         print(f"Error occurs : {err}")
 
+def updatefile():
+    try:
+        allfile()
+        name = input("Tell which file you want to update :- ")
+        p = Path(name)
+        if p.exists() and p.is_file():
+            print("Press 1 for changing the file name :- ")
+            print("Press 2 for overwriting the data in the file :- ")
+            print("Press 3 for appending some content in your file :- ")
+
+            res = int(input("Tell your response :- "))
+
+            if res == 1:
+                name2 = input("Tell your new file name :- ")
+                p2 = Path(name2)
+                p.rename(p2)
+
+            if res == 2:
+                with open(p, 'w') as fu:
+                    data = input("Please give content for what you want to overwrite in file :- ")
+                    fu.write(data)
+
+            if res == 3:
+                with open(p, 'a') as fa:
+                    data = input("Tell what you want to append:- ")
+                    fa.write(data)
+                    
+    except Exception as err:
+        print(f"Error occurs :- {err}")
+
+def deletefile():
+    try:
+        allfile()
+        name = input("Which file do you want to delete :- ")
+        p = Path(name)
+
+        if p.exists() and p.is_file():
+            os.remove(p)
+            print("File removed successfuly")
+
+        else:
+            print("No file exist.")
+
+    except Exception as err:
+        print(f"Error occurs :- {err}")
+
 print("Press 1 for creating a file")
 print("Press 2 for updating a file")
 print("Press 3 for delete a file")
@@ -47,6 +95,12 @@ check = int(input("Please tell your response :- "))
 
 if check == 1: 
     createfile()
+
+if check == 2:
+    updatefile()
+
+if check == 3:
+    deletefile()
 
 if check == 4:
     readfile()
