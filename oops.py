6b9e6a8c8029e07331f3506433432830ca26dc9e
Tejas <tejasspatell2@gmail.com>
2025-07-31 16:55:11 +0530

    learnig oops

--- oops.py
+++ oops.py
@@ -28,3 +28,26 @@ Student4 = Students("Pratik", "C", 66)
 print(Student4.name)
 print(Student2.rollNo, Student2.name)
 
+#! Types of attributes
+class car:
+    wheel = "lione"               #! Class attribute (ye sab cars ke liye common hai)
+
+    def __init__(self, speed):    #! Constructor (jab object banta hai tab run hota hai)
+        self.speed = speed        #! Instance attribute (ye sirf us object ke liye hota hai)
+
+    def show(self):               #! Instance method (object se call hota hai)
+        print(f"Car speed is :- {self.speed}")
+
+    @classmethod
+    def hello(cls):               #! Class method (class ya object dono se call ho sakta hai)
+        # print(f"How are you :- {cls.speed}") #? It can't access constructor or instance attribute
+        print(f"How are you :- {cls.wheel}")   
+
+    @staticmethod
+    def static():                 #! Static method (normal function jaisa, na class ka na object ka)
+        print("This is static method.")
+
+obj = car("45km/h")
+obj.show()
+car.hello()
+obj.hello()
